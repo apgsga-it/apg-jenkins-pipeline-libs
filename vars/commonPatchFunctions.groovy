2d3bb6096efb5bc563252e415ffcbd86024ff514
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-10-01 13:05:34 +0200

    reading a json file

--- vars/commonPatchFunctions.groovy
+++ vars/commonPatchFunctions.groovy
@@ -1,3 +1,10 @@
+import groovy.json.JsonSlurperClassic
+
 def printTestMessage(def param) {
     println "This is a test message ... with param = ${param}"
+}
+
+def readPatchJsonFile(def jsonFile) {
+    def patchConfig = new JsonSlurperClassic().parseText(jsonFile.text)
+    patchConfig
 }
