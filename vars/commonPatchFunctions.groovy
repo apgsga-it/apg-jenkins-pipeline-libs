b56a80b9dc5126debe59cc72c87d598843096918
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-10-06 11:13:16 +0200

    dealing with a service not existing witin Revisions.json

--- vars/commonPatchFunctions.groovy
+++ vars/commonPatchFunctions.groovy
@@ -12,7 +12,14 @@ def readPatchJsonFile(def jsonFile) {
 // TODO JHE (06.10.2020): This might/could be centralized somewhere else
 def getRevisionFor(service,target) {
     def jsonFile = new File("/var/jenkins/gradle/home/Revisions.json")
+    if(jsonFile == null) {
+        println "Current Revision for ${service.serviceName} on ${target} = SNAPSHOT"
+        return "SNAPSHOT"
+    }
     def json = readPatchJsonFile(jsonFile)
+    if(json.services."${service.serviceName}" == null || json.services."${service.serviceName}"."${target}") {
+        println "Current Revision for ${service.serviceName} on ${target} = SNAPSHOT"
+    }
     def revision = json.services."${service.serviceName}"."${target}"
     println "Current Revision for ${service.serviceName} on ${target} = ${revision}"
     return revision
