4a245964772d2b722cab312f4b0de64deced6d03
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-10-09 08:48:17 +0200

    dbAssemble

--- vars/patchfunctions.groovy
+++ vars/patchfunctions.groovy
@@ -18,9 +18,42 @@ def patchBuildsConcurrent(patchConfig) {
             lock("dbBuild-${patchConfig.currentTarget}-Build") {
                 deleteDir()
                 coDbModules(patchConfig)
+                dbAssemble(patchConfig)
             }
         }
     }
+}
+
+def dbAssemble(patchConfig) {
+    def PatchDbFolderName = getCoPatchDbFolderName(patchConfig)
+    fileOperations ([
+            folderCreateOperation(folderPath: "${PatchDbFolderName}\\config")
+    ])
+    // Done in order for the config folder to be taken into account when we create the ZIP...
+    fileOperations ([
+            fileCreateOperation(fileName: "${PatchDbFolderName}\\config\\dummy.txt", fileContent: "")
+    ])
+    def cmPropertiesContent = "config_name:${PatchDbFolderName}\r\npatch_name:${PatchDbFolderName}\r\ntag_name:${PatchDbFolderName}"
+    fileOperations ([
+            fileCreateOperation(fileName: "${PatchDbFolderName}\\cm_properties.txt", fileContent: cmPropertiesContent)
+    ])
+    def configInfoContent = "config_name:${PatchDbFolderName}"
+    fileOperations ([
+            fileCreateOperation(fileName: "${PatchDbFolderName}\\config_info.txt", fileContent: configInfoContent)
+    ])
+
+    def installPatchContent = "@echo off\r\n"
+    // TODO (jhe) :  0900C info doesn't exist at the moment witin patchConfig... also datetime ... do we have it somewhere?
+    installPatchContent += "@echo *** Installation von Patch 0900C_${patchConfig.patchNummer} [Build von TODO get YYYY/MM/dd-HH:mm:ss]\r\n"
+    installPatchContent += "set /p v_params=Geben Sie die Zielumgebung ein: \r\n"
+    installPatchContent += "pushd %~dp0 \r\n\r\n"
+    installPatchContent += "cmd /c \\\\cm-linux.apgsga.ch\\cm_ui\\it21_patch.bat %v_params%\r\n"
+    installPatchContent += "popd"
+    fileOperations ([
+            fileCreateOperation(fileName: "${PatchDbFolderName}\\install_patch.bat", fileContent: installPatchContent)
+    ])
+
+    publishDbAssemble(patchConfig)
 }
 
 def coDbModules(patchConfig) {
