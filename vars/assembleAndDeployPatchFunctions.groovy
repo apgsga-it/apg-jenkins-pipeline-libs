fd6dfc5d95bd8943c7a937c38673edbf987f588e
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-12-09 09:35:19 +0100

    log done in pipeline

--- vars/assembleAndDeployPatchFunctions.groovy
+++ vars/assembleAndDeployPatchFunctions.groovy
@@ -1,7 +1,6 @@
 #!groovy
 
 def assembleAndDeploy(target,parameter) {
-    commonPatchFunctions.log("assembleAndDeploy Job will be started for ${target} with following parameter ${parameter}","assembleAndDeploy")
     assertParameter(parameter)
     logPatchActivity(parameter.patches,target)
     checkoutPackagerProjects(parameter.gradlePackagerProjectAsVscPath)
