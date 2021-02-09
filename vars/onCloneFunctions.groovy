4bd44128a1ec37e5c8f4042c0612a030be9db99b
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-02-09 06:43:45 +0100

    typo on gradle command

--- vars/onCloneFunctions.groovy
+++ vars/onCloneFunctions.groovy
@@ -16,7 +16,7 @@ def resetRevisionFor(params) {
 
     dir("cm-resetRevision") {
         sh "chmod +x ./gradlew"
-        def cmd = "./gradlew ./gradlew resetRevision -PserviceName=echoservice -Psrc=chei211 -Ptarget=CHEI212 ${env.GRADLE_OPTS} --info --stacktrace"
+        def cmd = "./gradlew resetRevision -PserviceName=echoservice -Psrc=chei211 -Ptarget=CHEI212 ${env.GRADLE_OPTS} --info --stacktrace"
         def result = sh ( returnStdout : true, script: cmd).trim()
         println "result of ${cmd} : ${result}"
     }
