7127a2d1417e27f599c96f2ea382c5ec3f458489
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-02-10 08:39:46 +0100

    avoid using CVS gradle project

--- vars/onCloneFunctions.groovy
+++ vars/onCloneFunctions.groovy
@@ -1,6 +1,19 @@
 #!groovy
 
 def resetRevisionFor(params) {
+
+    deleteDir()
+    def buildGradle = libraryResource("build.gradle.resetRevision")
+    writeFile(file: "build.gradle", text: buildGradle)
+    params.buildParameters.each { bp ->
+        bp.services.each { s ->
+            def cmd = "gradle resetRevision -PserviceName=${s.serviceName} -Psrc=${params.src} -Ptarget=${params.target} ${env.GRADLE_OPTS} --info --stacktrace"
+            def result = sh(returnStdout: true, script: cmd).trim()
+            println "result of ${cmd} : ${result}"
+        }
+    }
+
+    /*
     deleteDir()
     commonPatchFunctions.coFromBranchCvs(env.CM_RESET_REV_PROJECT_CVS_BRANCH, "cm-resetRevision")
     dir("cm-resetRevision") {
@@ -13,4 +26,6 @@ def resetRevisionFor(params) {
             }
         }
     }
+
+     */
 }
