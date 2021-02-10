be2eef53b16507408e3c1410e5749faa09779e38
Julien Helbling <Julien.Helbling@apgsga.ch>
2021-02-10 13:07:20 +0100

    solving libraryResource problem

--- vars/onCloneFunctions.groovy
+++ vars/onCloneFunctions.groovy
@@ -13,8 +13,12 @@ def resetRevisionFor(params) {
 }
 
 def getResetRevisionGradleFile() {
-    // JHE (10.02.2021) : No idea why libraryResource function doesn't work. As a workaround, we're getting the resource using wget
-    def cmd = "wget https://github.com/apgsga-it/apg-jenkins-pipeline-libs/tree/${env.GITHUB_JENKINS_VERSION}/resources/build.gradle.resetRevision -o build.gradle"
+    // JHE (10.02.2021) : No idea why libraryResource function doesn't work. As a workaround, we clone the repo and take what we want from there
+    //                    Probably a wget from following would also work: https://raw.githubusercontent.com/apgsga-it/apg-jenkins-pipeline-libs/${env.GITHUB_JENKINS_VERSION}/resources/build.gradle.resetRevision
+    def cmd = "git clone -b ${env.GITHUB_JENKINS_VERSION} https://github.com/apgsga-it/apg-jenkins-pipeline-libs.git"
     def result = sh(returnStdout: true, script: cmd).trim()
     println "result of ${cmd} : ${result}"
+    def renameCmd = "mv resources/build.gradle.resetRevision build.gradle"
+    def renameCmdResult = sh(returnStdout: true, script: cmd).trim()
+    println "result of ${renameCmd} : ${renameCmdResult}"
 }
